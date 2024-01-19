042b1050198d0a6c37c19fb7d144b35450af2134
Siim <siim.kirjanen@gmail.com>
2024-01-19 17:47:52 +0200

    fix failing e2e test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,7 @@
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/');
-    cy.contains('h1', 'Welcome to the Nuxt Playground');
+    cy.contains('h1', 'Welcome');
+    cy.contains('p', 'Examples of Nuxt usage');
   });
 });
